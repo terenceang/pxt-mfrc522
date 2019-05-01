324e9f8c58f42ba87a94d2e5f4093fa23bb0d1aa
Terence Ang <terenceang@mac.com>
2019-05-01 13:00:33 +0800

    commit

--- mfrc522.ts
+++ mfrc522.ts
@@ -2,4 +2,8 @@
 //% color=#f44242 icon="\uf185"
 namespace mfrc522 {
 
+    //% weight=30 blockId = "RFID_init" block = "MFRC522"
+    export function rfid_init(sck: DigitalPin, mosi: DigitalPin, miso: Digital ): number {
+
+    }
 }
